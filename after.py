50942f5b13d00b93c35df4a9917f12f285d5b2e7
Aditya Pratap Singh <adityarhcsa@gmail.com>
2025-07-13 17:26:18 +0530

    adding payroll

--- after.py
+++ after.py
@@ -9,6 +9,8 @@ from typing import List
 
 FIXED_VACATION_DAYS_PAYOUT = 5  # The fixed nr of vacation days that can be paid out.
 
+class Payroll:
+    pass
 
 class VacationDaysShortageError(Exception):
     """Custom error that is raised when not enough vacation days are available."""
